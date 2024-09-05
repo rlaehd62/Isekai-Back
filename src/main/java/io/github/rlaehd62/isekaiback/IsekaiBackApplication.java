5040da3b15cfa1bae99e14c54db9440cffadec2d
rlaehd62@gmail.com <29157441+rlaehd62@users.noreply.github.com>
2024-09-05 16:33:08 +0900

    Add: Initialization

---
+++ src/main/java/io/github/rlaehd62/isekaiback/IsekaiBackApplication.java
@@ -0,0 +1,15 @@
+package io.github.rlaehd62.isekaiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsekaiBackApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(IsekaiBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/rlaehd62/isekaiback/IsekaiBackApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.rlaehd62.isekaiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsekaiBackApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
